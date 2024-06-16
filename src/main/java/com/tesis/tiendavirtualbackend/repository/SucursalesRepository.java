8f6d704d986f96bb1cc367d5ef7333d0a0d682fa
LestherHD <wmentel@miumg.edu.gt>
2024-06-15 22:25:03 -0600

    Correccion de query para consulta de departamento en sucursales

--- src/main/java/com/tesis/tiendavirtualbackend/repository/SucursalesRepository.java
+++ src/main/java/com/tesis/tiendavirtualbackend/repository/SucursalesRepository.java
@@ -15,7 +15,7 @@ public interface SucursalesRepository extends JpaRepository<Sucursales, Long> {
             "FROM Sucursales s " +
             "WHERE (?1 is null or CAST(s.id as string) LIKE %?2%) " +
             "and (?3 is null or s.nombre like %?4%) " +
-            "and (?5 is null or s.departamento like %?6%)")
+            "and (?5 is null or s.departamento = ?6)")
     Page<Sucursales> getByPage(Long idSource, Long id, String nombreSource, String nombre,
                                String departamentoSource, String departamento,
                                Pageable pageable);
